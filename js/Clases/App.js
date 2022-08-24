96e1c5b4e880714e7fba674a54032f1c0b389a71
covajg <josegregorio.covag@gmail.com>
2022-08-24 08:32:41 +0200

    primer commit

---
+++ js/Clases/App.js
@@ -0,0 +1,27 @@
+import { datosCita, nuevaCita } from '../funciones.js';
+import { mascotaInput, 
+    propietarioInput, 
+    telefonoInput, 
+    fechaInput, 
+    horaInput, 
+    sintomasInput, 
+    formulario
+} from '../selectores.js';
+
+export default class App {
+    constructor(){
+        this.initApp();
+    }
+
+    initApp() {
+        mascotaInput.addEventListener('change', datosCita);
+        propietarioInput.addEventListener('change', datosCita);
+        telefonoInput.addEventListener('change', datosCita);
+        fechaInput.addEventListener('change', datosCita);
+        horaInput.addEventListener('change', datosCita);
+        sintomasInput.addEventListener('change', datosCita);
+
+        //Formulario para nuevas citas
+        formulario.addEventListener('submit', nuevaCita);
+    }
+}
